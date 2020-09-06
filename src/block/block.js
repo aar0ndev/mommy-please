e079db0cfb2d179c0eb68b8be6d1636a361279ce
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2020-09-05 23:47:04 -0500

    feat: polish block page look

--- src/block/block.js
+++ src/block/block.js
@@ -1,4 +1,8 @@
 /* global chrome */
+import { getDomain } from '../util.js'
+import { Log, LogLevel } from '../log.js'
+
+const log = new Log('block', LogLevel.DEBUG)
 
 var input = document.querySelector('input.password')
 var duration = document.querySelector('select.duration')
@@ -9,7 +13,13 @@ function uiSetState (val) {
 }
 
 function uiShowError (msg) {
-  errorContainer.innerText = msg
+  if (msg.includes('options')) {
+    msg = msg.replace(
+      'options',
+      '<a href="/options/options.html" target="options">options</a>'
+    )
+  }
+  errorContainer.innerHTML = msg
 }
 
 function uiClearError () {
@@ -17,9 +27,14 @@ function uiClearError () {
 }
 
 function unblockResponseCallback (response) {
+  log.debug({ method: 'unblockResponseCallback', response })
   if (response.result === true) {
     uiSetState('done')
     window.location.replace(response.url)
+    setTimeout(() => {
+      log.debug({ comment: 'forcing url change', response })
+      window.location.href = response.url
+    }, 2000)
   } else if (response.error) {
     uiSetState('confirm')
     uiShowError(response.error)
@@ -27,11 +42,13 @@ function unblockResponseCallback (response) {
 }
 
 function askMommyHandler (e) {
+  log.debug({ method: 'askMommyHandler', e })
   uiSetState('confirm')
   input.focus()
 }
 
 function unblockHandler (e) {
+  log.debug({ method: 'askMommyHandler', e })
   e.preventDefault()
   uiClearError()
   var url = window.location.hash.substr(1)
@@ -50,3 +67,9 @@ document
 document
   .querySelector('button.unblock')
   .addEventListener('click', unblockHandler)
+
+document.querySelector('.domain').innerText = getDomain(
+  window.location.hash.slice(1)
+)
+
+log.prune()
