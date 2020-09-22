6bc1e964720c50064678a0de44976edbdc1d0ed6
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2020-09-22 07:43:40 -0500

    fix: refactor log to avoid this

--- extension/src/common/log.js
+++ extension/src/common/log.js
@@ -7,70 +7,70 @@ const LogLevel = Object.freeze({
 
 const MAX_LOG_SIZE = 100
 
-class Log {
-  constructor (name, level) {
-    name = name || 'default'
-    this.logKey = '_log_' + name
-    level = level || LogLevel.DEBUG
-    this.level = level
+function Log (name, level) {
+  const _ = {}
+  _.name = name || 'default'
+  _.logKey = '_log_' + name
+  setLevel(level || LogLevel.INFO)
+
+  return { debug, info, warn, error, clear, prune, setLevel }
+
+  function debug (data) {
+    _log(LogLevel.DEBUG, data)
   }
 
-  _log (level, data) {
-    if (level.val < this.level.val) {
+  function info (data) {
+    _log(LogLevel.INFO, data)
+  }
+
+  function warn (data) {
+    _log(LogLevel.WARN, data)
+  }
+
+  function error (data) {
+    _log(LogLevel.ERROR, data)
+  }
+
+  function clear () {
+    localStorage.removeItem(_.logKey)
+  }
+
+  function prune () {
+    const logList = _getAll()
+    if (logList.length > MAX_LOG_SIZE) {
+      _updateAll(logList.slice(logList.length - MAX_LOG_SIZE))
+    }
+  }
+
+  function setLevel (level) {
+    if (level && level.name in LogLevel) {
+      _.level = LogLevel[level.name]
+    } else {
+      throw new Error('LogLevel not valid')
+    }
+  }
+
+  function _getAll () {
+    return JSON.parse(localStorage.getItem(_.logKey) || '[]')
+  }
+
+  function _updateAll (logData) {
+    localStorage.setItem(_.logKey, JSON.stringify(logData))
+  }
+
+  function _log (level, data) {
+    if (level.val < _.level.val) {
       return
     }
     console.debug(`[${level.name}]`, data)
-    const logList = this._getAll()
+    const logList = _getAll()
 
     // try to guess if an error was passed
     if (data.stack && data.message) {
       data = { error: data.message }
     }
     logList.push({ date: new Date(), level: level.name, data })
-    this._updateAll(logList)
-  }
-
-  setLevel (level) {
-    if (level in LogLevel) {
-      this.level = level
-    } else {
-      throw new Error('LogLevel not valid')
-    }
-  }
-
-  debug (data) {
-    this._log(LogLevel.DEBUG, data)
-  }
-
-  info (data) {
-    this._log(LogLevel.INFO, data)
-  }
-
-  warn (data) {
-    this._log(LogLevel.WARN, data)
-  }
-
-  error (data) {
-    this._log(LogLevel.ERROR, data)
-  }
-
-  _getAll () {
-    return JSON.parse(localStorage.getItem(this.logKey) || '[]')
-  }
-
-  _updateAll (logData) {
-    localStorage.setItem(this.logKey, JSON.stringify(logData))
-  }
-
-  clear () {
-    localStorage.removeItem(this.logKey)
-  }
-
-  prune () {
-    const logList = this._getAll()
-    if (logList.length > MAX_LOG_SIZE) {
-      this._updateAll(logList.slice(logList.length - MAX_LOG_SIZE))
-    }
+    _updateAll(logList)
   }
 }
 
