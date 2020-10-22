ca070cbe3f2786cdd3dd84eeaf5525d33467bf4f
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2020-10-22 07:32:19 -0500

    feat: add first unit test

---
+++ extension/src/__test__/whitelist.spec.js
@@ -0,0 +1,8 @@
+/* global describe, it, expect */
+import * as whitelist from '../background/whitelist.js'
+
+describe('whitelist', () => {
+  it('exists', () => {
+    expect(whitelist).not.toBe(null)
+  })
+})
